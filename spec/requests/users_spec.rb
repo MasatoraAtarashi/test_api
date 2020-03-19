22998b23679f2a1aacc3430c5226f24fd78d7c05
あたらしまさとら <masatora.atarashi@gmail.com>
2020-03-19 09:55:08 +0900

    Complete Tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe 'Users API', type: :request do
     perfect_params = {'user_id': 'TaroYamada', 'password': 'PaSSwd4TY', 'nickname': 'たろー', 'comment': '僕は元気です'}
     post '/signup', params: { user: perfect_params }
 
-    valid_params = {'user_id': 'masatora', 'password': 'password'}
-    post '/signup', params: { user: valid_params }
+    masatora_params = {'user_id': 'masatora', 'password': 'password'}
+    post '/signup', params: { user: masatora_params }
   end
 
   describe 'POST /signup' do
     it 'userを作成する' do
-      valid_params = {'user_id': 'userid', 'password': 'password'}
+        valid_params = {'user_id': 'userid', 'password': 'password'}
       expect { post '/signup', params: { user: valid_params } }.to change(User, :count).by(+1)
       expect(response.status).to eq(200)
     end
@@ -127,37 +127,132 @@ RSpec.describe 'Users API', type: :request do
   end
 
   describe 'PATCH /users/{user_id}' do
+    valid_header = { 'Authorization' => ActionController::HttpAuthentication::Basic.encode_credentials("TaroYamada","PaSSwd4TY") }
+    valid_params = { 'nickname': 'unkoman', 'comment': 'unkomandesu' }
+
     it '成功' do
+      patch '/users/TaroYamada', headers: valid_header, params: valid_params
+      expect(response.status).to eq(200)
+      expected = {
+        "message": "User successfully updated",
+        "recipe": [
+          {
+            "nickname": "unkoman",
+            "comment": "unkomandesu"
+          }
+        ]
+      }.to_json
+      response.body.should == expected
     end
 
     it '指定user_idのユーザ情報が存在しない場合' do
+      invalid_header = { 'Authorization' => ActionController::HttpAuthentication::Basic.encode_credentials("anonymass","password") }
+      path '/users/anonymass', headers: invalid_header, params: valid_params
+      json = JSON.parse(response.body)
+      expect(response.status).to eq(404)
+      expected = { "message": "No User found" }.to_json
+      response.body.should == expected
     end
 
     it 'nickname と comment が両方とも指定されていない場合' do
+      invalid_params = {}
+      path '/users/TaroYamada', headers: valid_header, params: invalid_params
+      json = JSON.parse(response.body)
+      expect(response.status).to eq(400)
+      expected = {
+        "message": "User updation failed",
+        "cause": "required nickname or comment"
+      }.to_json
+      response.body.should == expected
     end
 
     it 'user_id や password を変更しようとしている場合' do
+      invalid_params = {'user_id': 'userid', 'password': 'password'}
+      path '/users/TaroYamada', headers: valid_header, params: invalid_params
+      json = JSON.parse(response.body)
+      expect(response.status).to eq(400)
+      expected = {
+        "message": "User updation failed",
+        "cause": "not updatable user_id and password"
+      }.to_json
+      response.body.should == expected
     end
 
     it 'Authorizationヘッダでの認証が失敗した場合' do
+      invalid_header = { 'Authorization' => ActionController::HttpAuthentication::Basic.encode_credentials("TaroYamada","password") }
+      patch '/users/TaroYamada', headers: invalid_header, params: valid_params
+      expect(response.status).to eq(401)
+      expected = { "message":"Authentication Faild" }.to_json
+      response.body.should == expected
     end
 
     it '認証と異なるIDのユーザを指定した場合' do
+      patch '/users/masatora', headers: valid_header, params: valid_params
+      expect(response.status).to eq(403)
+      expected = { "message": "No Permission for Update" }.to_json
+      response.body.should == expected
     end
 
-    it 'nickname30文字以上指定した場合' do
+    it 'nickname31文字以上指定した場合' do
+      patch '/users/TaroYamada', headers: valid_header, params: { 'nickname': 'p' * 31 }
+      expect(response.status).to eq(400)
     end
 
     it 'nickname空文字' do
+      patch '/users/TaroYamada', headers: valid_header, params: { 'nickname': ' ' }
+      expect(response.status).to eq(200)
+      get '/users/TaroYamada', headers: { 'Authorization' => ActionController::HttpAuthentication::Basic.encode_credentials("TaroYamada","PaSSwd4TY") }
+      json = JSON.parse(response.body)
+      expect(response.status).to eq(200)
+      expected = {
+        "message": "User details by user_id",
+        "user": {
+          "user_id": "TaroYamada",
+          "nickname": "たろー",
+          "comment": "僕は元気です"
+          }
+      }.to_json
+      response.body.should == expected
     end
 
-    it 'comment100文字以上指定した場合' do
+    it 'comment101文字以上指定した場合' do
+      patch '/users/TaroYamada', headers: valid_header, params: { 'comment': 'p' * 101 }
+      expect(response.status).to eq(400)
     end
 
     it 'comment空文字' do
+      patch '/users/TaroYamada', headers: valid_header, params: { 'comment': 'a a a' }
+      expect(response.status).to eq(200)
+      get '/users/TaroYamada', headers: { 'Authorization' => ActionController::HttpAuthentication::Basic.encode_credentials("TaroYamada","PaSSwd4TY") }
+      json = JSON.parse(response.body)
+      expect(response.status).to eq(200)
+      expected = {
+        "message": "User details by user_id",
+        "user": {
+          "user_id": "TaroYamada",
+          "nickname": "たろー",
+          "comment": "aaa"
+          }
+      }.to_json
+      response.body.should == expected
     end
   end
 
   describe 'POST /close' do
+    it '成功' do
+      valid_header = { 'Authorization' => ActionController::HttpAuthentication::Basic.encode_credentials("TaroYamada","PaSSwd4TY") }
+      expect { post '/close', headers: valid_header }.to change(User, :count).by(-1)
+      expect(response.status).to eq(200)
+      expected = {  "message": "Account and user successfully removed" }.to_json
+      response.body.should == expected
+    end
+
+    it 'Authorizationヘッダでの認証が失敗した場合' do
+      invalid_header = { 'Authorization' => ActionController::HttpAuthentication::Basic.encode_credentials("TaroYamada","password") }
+      expect { post '/close', headers: invalid_header }.to change(User, :count).by(-0)
+      expect(response.status).to eq(401)
+      expected = { "message":"Authentication Faild" }.to_json
+      response.body.should == expected
+    end
   end
 end
